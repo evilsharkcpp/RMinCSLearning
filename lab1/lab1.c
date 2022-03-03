8cd375196af851f909749e43aa1ec0eb23f101c6
evilsharkcpp <49671793+evilsharkcpp@users.noreply.github.com>
2022-03-03 23:59:38 +0700

    Updated

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -5,90 +5,92 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 
- void print(struct dirent** listDirs, int listSize)
- {
-    for(int i = 0; i < listSize; i++)
-      printf("|_%s\n",listDirs[i]->d_name);
-   printf("\n");
- }
-
- void sort(struct dirent** listDirs, int listSize)
- {
-    struct stat statbuf1, statbuf2;
-    for(int i = 0; i < listSize; i++)
-      for (int j = 0; j < listSize; j++)
-      {
-         lstat(listDirs[i]->d_name, &statbuf1);
-         lstat(listDirs[j]->d_name, &statbuf2);
-         if(statbuf1.st_mtim.tv_sec > statbuf2.st_mtim.tv_sec)
-         {
-            struct dirent* tmp = listDirs[j];
-            listDirs[j] = listDirs[i];
-            listDirs[i]= tmp;
-         }
-
-      }
- }
-
- int traverse(char *dirName)
- {
-   int maxListSize = 255;
-   static int totalDirs = 0;
-   DIR *dir_p = opendir(dirName);
-   struct dirent *entry;
-   struct stat statbuf;
-   struct dirent** listDirs = malloc(sizeof(struct dirent*)*maxListSize);
-
-   if (dir_p == NULL)
-   {
-	   fprintf(stderr, "Can`t open directory %s\n", dirName);
-		return 0;
-	}
-   chdir(dirName);
-   totalDirs++;
-   int listSize = 0;
-   while ((entry = readdir(dir_p)) != NULL) 
-   {
-      if(listSize > maxListSize)
-      {
-         maxListSize*=2;
-         if(realloc(listDirs,sizeof(struct dirent*)*maxListSize)==NULL)
-         {
-            fprintf(stderr, "No memory");
-            return 0;
-         }
-      }
-		lstat(entry->d_name, &statbuf);
-		if (S_ISDIR(statbuf.st_mode)) 
-      {
-			if (strcmp(entry->d_name, ".") == 0 || 
-				strcmp(entry->d_name, "..") == 0 )  
-				continue;
-         listDirs[listSize++] = entry; 
-		} 
-	}
-   if(listSize!=0)
-   {
-      printf("Directory name: %s\n",dirName);
-      sort(listDirs,listSize);
-      print(listDirs,listSize);
-
-      for(int i = 0; i < listSize; i++)
-         traverse(listDirs[i]->d_name);
-   }
-   chdir("..");
-	closedir(dir_p);
-   free(listDirs);
-   return totalDirs;
- }
- 
-
-
-int main(int argc, char *argv[])
+void print(struct dirent** listDirs, int listSize)
 {
-	char *topdir = ".";
-	if (argc >= 2)
-		topdir = argv[1];
-   printf("\nTotal directories: %d\n",traverse(topdir));
-	exit(0);
+    for (int i = 0; i < listSize; i++)
+        printf("|_%s\n", listDirs[i]->d_name);
+    printf("\n");
+}
+
+void sort(struct dirent** listDirs, int listSize)
+{
+    struct stat statbuf1, statbuf2;
+    for (int i = 0; i < listSize; i++)
+        for (int j = 0; j < listSize; j++)
+        {
+            lstat(listDirs[i]->d_name, &statbuf1);
+            lstat(listDirs[j]->d_name, &statbuf2);
+            if (statbuf1.st_mtim.tv_sec > statbuf2.st_mtim.tv_sec)
+            {
+                struct dirent* tmp = listDirs[j];
+                listDirs[j] = listDirs[i];
+                listDirs[i] = tmp;
+            }
+
+        }
+}
+
+int traverse(char* dirName)
+{
+    int maxListSize = 255;
+    static int totalDirs = 0;
+    DIR* dir_p = opendir(dirName);
+    struct dirent* entry;
+    struct stat statbuf;
+    struct dirent** listDirs = malloc(sizeof(struct dirent*) * maxListSize);
+
+    if (dir_p == NULL)
+    {
+        fprintf(stderr, "Can`t open directory %s\n", dirName);
+        return -1;
+    }
+    chdir(dirName);
+    totalDirs++;
+    int listSize = 0;
+    while ((entry = readdir(dir_p)) != NULL)
+    {
+        if (listSize > maxListSize)
+        {
+            maxListSize *= 2;
+            if (realloc(listDirs, sizeof(struct dirent*) * maxListSize) == NULL)
+            {
+                fprintf(stderr, "No memory");
+                return -2;
+            }
+        }
+        lstat(entry->d_name, &statbuf);
+        if (S_ISDIR(statbuf.st_mode))
+        {
+            if (strcmp(entry->d_name, ".") == 0 ||
+                strcmp(entry->d_name, "..") == 0)
+                continue;
+            listDirs[listSize++] = entry;
+        }
+    }
+    if (listSize != 0)
+    {
+        printf("Directory name: %s\n", dirName);
+        sort(listDirs, listSize);
+        print(listDirs, listSize);
+
+        for (int i = 0; i < listSize; i++)
+            traverse(listDirs[i]->d_name);
+    }
+    chdir("..");
+    closedir(dir_p);
+    free(listDirs);
+    return totalDirs;
+}
+
+
+
+int main(int argc, char* argv[])
+{
+    char* topdir = ".";
+    if (argc >= 2)
+        topdir = argv[1];
+    int count = traverse(topdir);
+    if (count == -1) return -1;
+    printf("\nTotal directories: %d\n", count);
+    return 0;
 }
--- lab1/script.sh
+++ lab1/script.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#Рекурсивная функция для обхода эл-тов каталога и сортировки
-#по времени последнего изменения
+# Рекурсивная функция для обхода эл-тов каталога и сортировки
+# по времени последнего изменения
 function traverse()
 {
-    
-    list=($(ls -1td $1/*/))
+    # Из-за рекурсивного поиска и того, что мы проверяем все каталоги,
+    # игнорируем отсутствие каталога
+    list=($(ls -1td $1/*/ 2> >(grep -v 'No such file or directory' >&2)))
     if [ ${#list[@]} -lt 1 ]; then
         return
     fi
     echo ""
     echo -n "Directory name: " 
     basename $1
-    
     for folder in ${list[@]}
     do
         for i in ${list[@]}
@@ -23,16 +23,19 @@ function traverse()
             traverse "$folder"  
             count=$(($count + 1))   
     done
-    
 }
 function main()
 {
     count=0
-    traverse "$1"
+    top="."
+    if [ ! -z $1 ]; then
+        top="$1"
+        ls "$top"
+        if [ $? -ne 0 ]; then
+            return
+        fi
+    fi
+    traverse "$top"
     echo "Total Directories: $count"
 }
-if [ "$2" != "-a" ]; then
-    main "$1" 2>/dev/null
-else
-    main "$1"
-fi
+main "$1"
